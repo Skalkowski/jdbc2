7db08bff680603b35bafc32f7f6ded3c051e8b4c
Michal Skalkowski <michalskalkowski@gmail.com>
2013-11-18 14:17:50 +0100

    finalna wersja-tak mi sie wydaje

--- src/main/java/com/example/jdbcdemo/service/SumaManager.java
+++ src/main/java/com/example/jdbcdemo/service/SumaManager.java
@@ -1,91 +1,88 @@
 package com.example.jdbcdemo.service;
 
-import java.sql.Connection;				//polaczenie z baza danych sql
+import java.sql.Connection; //polaczenie z baza danych sql
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;  	//obiekt który reprezentuje skomplikowany sql
-import java.sql.ResultSet;				//tworzenie tabeli danych przechowującą to co jest w bazie danych
+import java.sql.PreparedStatement; //obiekt który reprezentuje skomplikowany sql
+import java.sql.ResultSet; //tworzenie tabeli danych przechowującą to co jest w bazie danych
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-
-
-
-
-
-
-
 import com.example.jdbcdemo.domain.Grabarz;
 import com.example.jdbcdemo.domain.Trumna;
+
 public class SumaManager {
 	private Connection connection;
 
 	private String url = "jdbc:hsqldb:hsql://localhost/workdb";
 	private boolean TrumnaTableExists = false;
 	private boolean GrabarzTableExists = false;
-	
-	private String createTableTrumna = "CREATE TABLE Trumna(id bigint IDENTITY, nazwa varchar(20), gatunek_drewna varchar(20), grabarz int REFERENCES grabarz(id_grabarz) null)";
+
+	private String createTableTrumna = "CREATE TABLE Trumna(id bigint IDENTITY, nazwa varchar(20), gatunek_drewna varchar(20), grabarz bigint REFERENCES grabarz(id_grabarz) null)";
 	private String createTableGrabarz = "CREATE TABLE Grabarz(id_grabarz bigint IDENTITY, imie varchar(20), nazwisko varchar(20))";
-	
+
 	private PreparedStatement addTrumna;
 	private PreparedStatement addGrabarz;
 	private PreparedStatement dropTrumna;
 	private PreparedStatement dropGrabarz;
 	private PreparedStatement getTrumna;
+	private PreparedStatement getGrabarz;
+	private PreparedStatement selectTrumna;
+	private PreparedStatement selectGrabarz;
+	private PreparedStatement getWhereTrumna;
+	private PreparedStatement deleteTrumna;
 
-	
-	
 	private Statement statement;
-	
-	
-	
-	public SumaManager(){
+
+	public SumaManager() {
 		try {
 			connection = DriverManager.getConnection(url);
 			statement = connection.createStatement();
-			
-			ResultSet rs = connection.getMetaData().getTables(null, null, null,null);
 
-			//sprawdzenie czy tabela istnieje
-			
+			ResultSet rs = connection.getMetaData().getTables(null, null, null,
+					null);
+
+			// sprawdzenie czy tabela istnieje
+
 			while (rs.next()) {
 				if ("Trumna".equalsIgnoreCase(rs.getString("TABLE_NAME"))) {
 					TrumnaTableExists = true;
-					
-				}
-				else if ("Grabarz".equalsIgnoreCase(rs.getString("TABLE_NAME"))){
+
+				} else if ("Grabarz".equalsIgnoreCase(rs
+						.getString("TABLE_NAME"))) {
 					GrabarzTableExists = true;
-					
-					
+
 				}
 			}
 			if (!GrabarzTableExists)
 				statement.executeUpdate(createTableGrabarz);
-			
+
 			if (!TrumnaTableExists)
 				statement.executeUpdate(createTableTrumna);
-				
-			
-		
-				
-			
-			
-			addTrumna = connection.prepareStatement("INSERT INTO Trumna (nazwa, gatunek_drewna, grabarz) VALUES (?, ?, ?);");
-			addGrabarz = connection.prepareStatement("INSERT INTO Grabarz (imie, nazwisko) VALUES (?, ?);");
+
+			addTrumna = connection
+					.prepareStatement("INSERT INTO Trumna (nazwa, gatunek_drewna, grabarz) VALUES (?, ?, ?);");
+			addGrabarz = connection
+					.prepareStatement("INSERT INTO Grabarz (imie, nazwisko) VALUES (?, ?);");
 			dropTrumna = connection.prepareStatement("DROP TABLE Trumna;");
 			dropGrabarz = connection.prepareStatement("DROP TABLE Grabarz;");
 			getTrumna = connection.prepareStatement("SELECT * FROM Trumna;");
+			getGrabarz = connection.prepareStatement("SELECT * FROM Grabarz;");
+			getWhereTrumna = connection.prepareStatement("SELECT * FROM Trumna WHERE grabarz = (?);");
+			selectTrumna = connection.prepareStatement("SELECT * FROM Trumna WHERE nazwa = (?);");
+			selectGrabarz = connection.prepareStatement("SELECT * FROM Grabarz WHERE nazwisko = (?);");
+			deleteTrumna = connection.prepareStatement("DELETE FROM Trumna WHERE grabarz = (?);");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 
-	//polaczenie z tabela
+	// polaczenie z tabela
 	Connection getConnection() {
 		return connection;
 	}
-	
-	//drop tabeli trumna
+
+	// drop tabeli trumna
 	void clearTrumna() {
 		try {
 			dropTrumna.execute();
@@ -94,6 +91,7 @@ public class SumaManager {
 		}
 	}
 
+	// drop tabeli grabarz
 	void clearGrabarz() {
 		try {
 			dropGrabarz.execute();
@@ -101,8 +99,10 @@ public class SumaManager {
 			e.printStackTrace();
 		}
 	}
+
+	// dodanie trumny
 	public void addTrumna(Trumna trumna) {
-		
+
 		try {
 			addTrumna.setString(1, trumna.getNazwa());
 			addTrumna.setString(2, trumna.getGatunek_drewna());
@@ -112,11 +112,12 @@ public class SumaManager {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
+	// dodagnie grabarza
 	public void addGrabarz(Grabarz grabarz) {
-		
+
 		try {
 			addGrabarz.setString(1, grabarz.getImie());
 			addGrabarz.setString(2, grabarz.getNazwisko());
@@ -126,11 +127,10 @@ public class SumaManager {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	
-	
+
+	// Select calkowity z tabeli trumna
 	public ArrayList<Trumna> getTrumna() {
 		ArrayList<Trumna> trumna = new ArrayList<Trumna>();
 
@@ -151,12 +151,83 @@ public class SumaManager {
 		return trumna;
 	}
 
-	
-	public String nazwaToString(int nr){
-		return "NAZWA_"+nr;
+	// select calkowity z tabeli grabarz
+	public ArrayList<Grabarz> getGrabarz() {
+		ArrayList<Grabarz> grabarz = new ArrayList<Grabarz>();
+
+		try {
+			ResultSet rs = getGrabarz.executeQuery();
+
+			while (rs.next()) {
+				Grabarz g = new Grabarz();
+				g.setId(rs.getLong("id_grabarz"));
+				g.setImie(rs.getString("imie"));
+				g.setNazwisko(rs.getString("nazwisko"));
+				grabarz.add(g);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return grabarz;
 	}
-	public String gatunekToString(int nr){
-		return "GATUNEK_"+nr;
+
+	// pobranie pojedynczej trumny
+	public ArrayList<Trumna> getWhereTrumna(Long id_grabarz) {
+		ArrayList<Trumna> trumna = new ArrayList<Trumna>();
+
+		try {
+			getWhereTrumna.setLong(1, id_grabarz);
+			ResultSet rs = getWhereTrumna.executeQuery();
+
+			while (rs.next()) {
+				Trumna t = new Trumna();
+				t.setNumer(rs.getInt(1));
+				t.setNazwa(rs.getString(2));
+				t.setGatunek_drewna(rs.getString(3));
+				t.setGrabarz(rs.getInt(4));
+				trumna.add(t);
+				
+			}
+			return trumna;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return null;
 	}
-	
+
+	// pobranie pojedynczej grabarza
+	public Grabarz selectGrabarz(String nazwisko) {
+		Grabarz grabarz = new Grabarz();
+
+		try {
+			selectGrabarz.setString(1, nazwisko);
+			ResultSet rs = selectGrabarz.executeQuery();
+
+			while (rs.next()) {
+				grabarz.setId(rs.getLong(1));
+				grabarz.setImie(rs.getString(2));
+				grabarz.setNazwisko(rs.getString(3));
+			
+				return grabarz;
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	public void deleteTrumny(long grabarz){
+		
+		try {
+			deleteTrumna.setLong(1, grabarz);
+			
+			deleteTrumna.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/test/java/com/example/jdbcdemo/service/SumaManagerTest.java
+++ src/test/java/com/example/jdbcdemo/service/SumaManagerTest.java
@@ -1,6 +1,7 @@
 package com.example.jdbcdemo.service;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.List;
 
@@ -8,74 +9,98 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-
-import com.example.jdbcdemo.domain.Trumna;
 import com.example.jdbcdemo.domain.Grabarz;
+import com.example.jdbcdemo.domain.Trumna;
 
 public class SumaManagerTest {
 	SumaManager sumaManager = new SumaManager();
-	
-	private final static String NAZWA_1 = "szczęśliwa noc";
-	private final static String GATUNEK_1 = "Dębowa";
+
+	private final static String NAZWA_1 = "szczesliwa noc";
+	private final static String GATUNEK_1 = "Debowa";
 	private final static String NAZWA_2 = "przytulny zakatek";
 	private final static String GATUNEK_2 = "sosna";
-	
-	
-	
-	@Test
-	public void before(){
-		
-		
-       
+	private final static String NAZWA_3 = "podziemne zacisze";
+	private final static String GATUNEK_3 = "Debowa";
+
+	String[][] trumienka = { { "szczesliwa noc", "Debowa" },
+			{ "przytulny zakatek", "sosna" }, { "podziemne zacisze", "Debowa" } };
+	String[][] grabarzek = { { "Jan", "Kowalski" }, { "Adam", "Nowak" } };
+
+	@Before
+	public void addAll() {
+		Grabarz grabarz1 = new Grabarz("Jan", "Kowalski");
+		Grabarz grabarz2 = new Grabarz("Adam", "Nowak");
+		sumaManager.addGrabarz(grabarz1);
+		sumaManager.addGrabarz(grabarz2);
+		Trumna trumna1 = new Trumna(NAZWA_1, GATUNEK_1, 0);
+		Trumna trumna2 = new Trumna(NAZWA_2, GATUNEK_2, 1);
+		Trumna trumna3 = new Trumna(NAZWA_3, GATUNEK_3, 1);
+		sumaManager.addTrumna(trumna1);
+		sumaManager.addTrumna(trumna2);
+		sumaManager.addTrumna(trumna3);
 	}
-	
-	
+
+	// sprawdzenie selecta trumny
 	@Test
-	public void checkAddingTrumna(){
-		 Grabarz grabarz1 = new Grabarz("Jan", "Kowalski");
-		 Grabarz grabarz2 = new Grabarz("Adam", "Nowak");
-	        sumaManager.addGrabarz(grabarz1);
-	        sumaManager.addGrabarz(grabarz2);
-	    Trumna trumna1 = new Trumna(NAZWA_1, GATUNEK_1, 0);
-        Trumna trumna2 = new Trumna(NAZWA_2, GATUNEK_2, 1);
-        sumaManager.addTrumna(trumna1);
-        sumaManager.addTrumna(trumna2);
-		
-		
-		
+	public void checkAddingTrumna() {
+
 		List<Trumna> trumny = sumaManager.getTrumna();
-		
-		
+		int i = 0;
 		for (Trumna e : trumny) {
-		
-		
-		
-		
-		
-		assertEquals(sumaManager.nazwaToString(1), e.getNazwa());
-		assertEquals(sumaManager.gatunekToString(1), e.getGatunek_drewna());
-		assertEquals(sumaManager.nazwaToString(2), e.getNazwa());
-		assertEquals(sumaManager.gatunekToString(2), e.getGatunek_drewna());
-		System.out.println("cos");
+			trumienka[i][0].equals(e.getNazwa());
+			assertEquals(trumienka[i][0], e.getNazwa());
+			assertEquals(trumienka[i][1], e.getGatunek_drewna());
+			++i;
 		}
 	}
 
-	
-	//sprawdzenie polaczenia
+	// sprawdzenie selecta grabarzy
 	@Test
-	public void checkConnection(){
+	public void checkAddingGrabarz() {
+		List<Grabarz> grabarze = sumaManager.getGrabarz();
+		int i = 0;
+		for (Grabarz e : grabarze) {
+			assertEquals(grabarzek[i][0], e.getImie());
+			assertEquals(grabarzek[i][1], e.getNazwisko());
+			++i;
+		}
+	}
+
+	// sprawdzenie polaczenia
+	@Test
+	public void checkConnection() {
 		assertNotNull(sumaManager.getConnection());
 	}
-	
-	
-	
-	
-	
-	
 
+	// usuwanie tabel po każdym tescie
+	@After
+	public void after() {
+		sumaManager.clearTrumna();
+		sumaManager.clearGrabarz();
+
+	}
 	
+	@Test
+	public void selectGrabarz() {
+		Grabarz grabarz = new Grabarz();
+		grabarz = sumaManager.selectGrabarz("Nowak");
+		
+		long idTmp = grabarz.getId();
+		
+		List<Trumna> trumny = sumaManager.getWhereTrumna(idTmp);
+		
+		//sprawdzenie czy sa 2 trumny
+		assertEquals(2, trumny.size());
+	}
 	
-	
-	
-	
+	@Test
+	public void deleteTrumna(){
+		Grabarz grabarz = new Grabarz();
+		grabarz = sumaManager.selectGrabarz("Nowak");
+		
+		long idTmp = grabarz.getId();
+		sumaManager.deleteTrumny(idTmp);
+		List<Trumna> trumny = sumaManager.getWhereTrumna(idTmp);
+		assertEquals(0, trumny.size());
+	}
 }
